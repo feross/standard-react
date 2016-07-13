44cefa790f1785527a4046842f1bcf8dd3237da1
Feross Aboukhadijeh <feross@feross.org>
2016-07-13 14:59:50 -0700

    Switch to simpler inheritance of configs

--- options.js
+++ options.js
@@ -1,31 +1,16 @@
 var eslint = require('eslint')
-var extend = require('xtend')
+var path = require('path')
 var pkg = require('./package.json')
 
-var configStandard = require('eslint-config-standard')
-var configStandardJsx = require('eslint-config-standard-jsx')
-var configStandardReact = require('eslint-config-standard-react')
-
-var config = extend(configStandard)
-config.plugins.push.apply(config.plugins, configStandardJsx.plugins)
-
-Object.keys(configStandardJsx.rules).forEach(function (key) {
-  config.rules[key] = configStandardJsx.rules[key]
-})
-
-Object.keys(configStandardReact.rules).forEach(function (key) {
-  config.rules[key] = configStandardReact.rules[key]
-})
-
 module.exports = {
-  eslint: eslint,
-  cmd: 'standard-react',
-  version: pkg.version,
-  homepage: pkg.homepage,
   bugs: pkg.bugs.url,
-  tagline: 'Use JavaScript Standard Style (React)',
+  cmd: 'standard-react',
+  eslint: eslint,
   eslintConfig: {
-    baseConfig: config
+    configFile: path.join(__dirname, 'eslintrc.json')
   },
-  formatter: 'Formatting is no longer included with standard. Install it separately: "npm install -g standard-format"'
+  formatter: '`standard-format` is no longer included with `standard`. Try using --fix instead. Or, install `standard-format` separately: "npm install -g standard-format"',
+  homepage: pkg.homepage,
+  tagline: 'Use JavaScript Standard Style (React)',
+  version: pkg.version
 }
